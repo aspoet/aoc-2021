f151e918c993199a0fc888e5bc7c901446407f86
aspoet <9062668+aspoet@users.noreply.github.com>
2021-12-01 20:34:38 +0100

    Day01 2

--- src/days/01/index.ts
+++ src/days/01/index.ts
@@ -1,46 +1,45 @@
 export function one(_input: string[]): number {
-    const input = _input.map(x => Number.parseInt(x, 10))  
-    let counter = 0;
-    for(let i=0; i<input.length/3; i++){ 
-        if(input[i] > input[i - 1]){
-            counter++
-        }
+  const input = _input.map((x) => Number.parseInt(x, 10));
+  let counter = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > input[i - 1]) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+export function two(_input: string[]): number {
+  const input = _input.map((x) => Number.parseInt(x, 10));
+  let counter = 0;
+  let denoisedInput = denoise(input);
+
+  function denoise(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      let chunk = array.slice(i, i + 3);
+      result.push(chunk);
+    }
+    return result;
+  }
+
+  function compareArray(array) {
+    for (let i = 1; i < array.length; i++) {
+      if (sumArr(array[i]) > sumArr(array[i - 1])) {
+        counter++;
+      }
     }
     return counter;
-}
-  
-export function two(_input: string[]): number {
+  }
 
-    const input = _input.map(x => Number.parseInt(x, 10));
-    let counter = 0;
-    let denoisedInput = denoise(input);
-
-    function denoise(array) {
-        let result = [];
-        for(let i = 0; i<array.length; i++) {
-            let chunk = array.slice(i,i+3)
-            result.push(chunk)
-        }
-            return result
-        }
-
-    function compareArray(array) {        
-        for(let i=1; i < array.length ; i++){ 
-            if(sumArr(array[i]) > sumArr(array[i-1])){
-                counter++
-            }
-        }
-        return counter
+  function sumArr(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
     }
-    
-    function sumArr(array) {
-        let sum = 0;
-        for (let i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-        return sum
-    }
+    return sum;
+  }
 
-  return compareArray(denoisedInput)
- 
+  return compareArray(denoisedInput);
 }
